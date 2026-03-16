21b7d14f51ef79809257e3df011bd39ca01306cb
sunilmaheshwaram <sunilmaheshwaram99@gmail.com>
2026-03-16 22:28:42 +0530

    Enhance Full_scan_insertion_script with phases
    Added setup, analysis, and insertion phases to the script.

--- Full_scan_insertion_script.tcl
+++ Full_scan_insertion_script.tcl
@@ -1,6 +1,24 @@
-# Script
-
+# Setup Phase
 set_context dft -scan
 read_verilog design.v
 read_cell_library dft.atgplib
+add_input_constraints
+add_clocks
+set_current_design
+set_design_level chip
+set_test_logic
+check_design_rules
+
+# Analysis Phase
+insert_test_logic
+report_scan_chains
+report_scan_clocks
+report_scan_elements
+
+# Insertion Phase
+write_design
+write_atpg_setup
+
+
+
 
